571e7c967517e1c42a4524e2e96165086a072002
Piotr Pepliński <p.peplaj@gmail.com>
2021-05-18 19:32:40 +0200

    displayed posts and created detail pages

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('<int:postId>', views.detail, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
 
 def home(request):
-    return render(request, 'home.html')
+    posts = Post.objects.all()
+    return render(request, 'home.html', {'posts': posts})
+
+
+def detail(request, postId):
+    post = get_object_or_404(Post, pk=postId)
+    return render(request, 'detail.html', {'post': post})
