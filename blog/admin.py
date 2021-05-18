faa768f539f828286fb770728fd5afd50cc34c27
Piotr Pepliński <p.peplaj@gmail.com>
2021-05-18 19:31:46 +0200

    created and registered Post model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+class DisplayDate(admin.ModelAdmin):
+    readonly_fields = ('createDate',)
+
+admin.site.register(Post, DisplayDate)
--- blog/models.py
+++ blog/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 #title, text, createDate
 class Post(models.Model):
-    pass
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    createDate = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title + ' ' + str(self.createDate)
